a09aa17270ae2b2f02acc0e8b0a69e1437e2a07e
TheTiredOne22 <120611653+TheTiredOne22@users.noreply.github.com>
2024-02-12 16:02:29 +0100

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mail.urls')),
     path('accounts/', include('allauth.urls')),
 
 ]
